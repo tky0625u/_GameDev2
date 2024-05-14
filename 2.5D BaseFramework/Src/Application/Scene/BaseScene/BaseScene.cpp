d5fb6c5e3c3ef785ccac919cca08b54076532918
tky0625u <kd1370302@st.kobedenshi.ac.jp>
2024-05-14 19:51:43 +0900

    クォータービュー
    準備コミット

---
+++ 2.5D BaseFramework/Src/Application/Scene/BaseScene/BaseScene.cpp
@@ -0,0 +1,142 @@
+﻿#include "BaseScene.h"
+
+void BaseScene::PreUpdate()
+{
+	// Updateの前の更新処理
+	// オブジェクトリストの整理 ・・・ 無効なオブジェクトを削除
+	auto it = m_objList.begin();
+
+	while (it != m_objList.end())
+	{
+		if ((*it)->IsExpired())	// IsExpired() ・・・ 無効ならtrue
+		{
+			// 無効なオブジェクトをリストから削除
+			it = m_objList.erase(it);
+		}
+		else
+		{
+			++it;	// 次の要素へイテレータを進める
+		}
+	}
+
+	// ↑の後には有効なオブジェクトだけのリストになっている
+}
+
+void BaseScene::Update()
+{
+	// KdGameObjectを継承した全てのオブジェクトの更新 (ポリモーフィズム)
+	for (auto& obj : m_objList)
+	{
+		obj->Update();
+	}
+
+	// シーン毎のイベント処理
+	Event();
+}
+
+void BaseScene::PostUpdate()
+{
+	for (auto& obj : m_objList)
+	{
+		obj->PostUpdate();
+	}
+}
+
+void BaseScene::PreDraw()
+{
+	// カメラ情報が無い場合は早期リターン
+	if (!m_camera)return;
+
+	// 現在のカメラ情報をシェーダーにセット
+	m_camera->SetToShader();
+
+	for (auto& obj : m_objList)
+	{
+		obj->PreDraw();
+	}
+}
+
+void BaseScene::Draw()
+{
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 光を遮るオブジェクト(不透明な物体や2Dキャラ)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_StandardShader.BeginGenerateDepthMapFromLight();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->GenerateDepthMapFromLight();
+		}
+	}
+	KdShaderManager::Instance().m_StandardShader.EndGenerateDepthMapFromLight();
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 陰影のあるオブジェクト(不透明な物体や2Dキャラ)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_StandardShader.BeginLit();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->DrawLit();
+		}
+	}
+	KdShaderManager::Instance().m_StandardShader.EndLit();
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 陰影のないオブジェクト(透明な部分を含む物体やエフェクト)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_StandardShader.BeginUnLit();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->DrawUnLit();
+		}
+	}
+	KdShaderManager::Instance().m_StandardShader.EndUnLit();
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 光源オブジェクト(自ら光るオブジェクトやエフェクト)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_postProcessShader.BeginBright();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->DrawBright();
+		}
+	}
+	KdShaderManager::Instance().m_postProcessShader.EndBright();
+}
+
+void BaseScene::DrawSprite()
+{
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 2Dの描画はこの間で行う
+	KdShaderManager::Instance().m_spriteShader.Begin();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->DrawSprite();
+		}
+	}
+	KdShaderManager::Instance().m_spriteShader.End();
+}
+
+void BaseScene::DrawDebug()
+{
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// デバッグ情報の描画はこの間で行う
+	KdShaderManager::Instance().m_StandardShader.BeginUnLit();
+	{
+		for (auto& obj : m_objList)
+		{
+			obj->DrawDebug();
+		}
+	}
+	KdShaderManager::Instance().m_StandardShader.EndUnLit();
+}
+
+void BaseScene::Event()
+{
+	// 各シーンで必要な内容を実装(オーバーライド)する
+}
+
+void BaseScene::Init()
+{
+	// 各シーンで必要な内容を実装(オーバーライド)する
+}
